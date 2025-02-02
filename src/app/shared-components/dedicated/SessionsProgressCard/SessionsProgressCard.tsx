a3a93aaa53bdfdb7e8872e5595dac58f5ce33974
JmZacapaDev <jmzacapa.dev@gmail.com>
2025-02-01 19:25:36 -0600

    Mi primer commit

--- src/app/shared-components/dedicated/SessionsProgressCard/SessionsProgressCard.tsx
+++ src/app/shared-components/dedicated/SessionsProgressCard/SessionsProgressCard.tsx
@@ -1,115 +1,29 @@
-import { Card, CardContent, Stack, Typography } from '@mui/material';
-import SessionProgress from 'app/shared-components/general/SessionProgress/SessionProgress';
+vzcbeg { Pneq, PneqPbagrag, Fgnpx, Glcbtencul } sebz '@zhv/zngrevny';
+vzcbeg FrffvbaCebterff sebz 'ncc/funerq-pbzcbaragf/trareny/FrffvbaCebterff/FrffvbaCebterff';
 
-export interface ISessionProgress {
-	id: number;
-	stars: number;
-	progress: number;
-	topic: Topic;
+rkcbeg vagresnpr VFrffvbaCebterff {
+	vq: ahzore;
+	fgnef: ahzore;
+	cebterff: ahzore;
+	gbcvp: Gbcvp;
 }
 
-interface Topic {
-	id: number;
-	name: string;
-	image: string;
-	subject: {
-		id: number;
-		name: string;
+vagresnpr Gbcvp {
+	vq: ahzore;
+	anzr: fgevat;
+	vzntr: fgevat;
+	fhowrpg: {
+		vq: ahzore;
+		anzr: fgevat;
 	};
 }
 
-const sessionsProgress: ISessionProgress[] = [
-	{
-		id: 1,
-		stars: 1.85,
-		progress: 90,
-		topic: {
-			id: 1,
-			name: 'Multiplicaciones y divisiones',
-			image: 'assets/images/lessons-images/lesson_image_maths.png',
-			subject: {
-				id: 1,
-				name: 'Matemáticas'
-			}
-		}
-	},
-	{
-		id: 2,
-		stars: 0.5,
-		progress: 20,
-		topic: {
-			id: 2,
-			name: 'Arte antiguo',
-			image: 'assets/images/lessons-images/lesson_image_arts.png',
-			subject: {
-				id: 2,
-				name: 'Historia'
-			}
-		}
-	},
-	{
-		id: 3,
-		stars: 0,
-		progress: 0,
-		topic: {
-			id: 3,
-			name: 'Pronouns',
-			image: 'assets/images/lessons-images/lesson_image_english.png',
-			subject: {
-				id: 3,
-				name: 'Lengua Extranjera'
-			}
-		}
-	},
-	{
-		id: 1,
-		stars: 1.85,
-		progress: 90,
-		topic: {
-			id: 1,
-			name: 'Multiplicaciones y divisiones',
-			image: 'assets/images/lessons-images/lesson_image_maths.png',
-			subject: {
-				id: 1,
-				name: 'Matemáticas'
-			}
-		}
-	},
-	{
-		id: 2,
-		stars: 0.5,
-		progress: 20,
-		topic: {
-			id: 2,
-			name: 'Arte antiguo',
-			image: 'assets/images/lessons-images/lesson_image_arts.png',
-			subject: {
-				id: 2,
-				name: 'Historia'
-			}
-		}
-	},
-	{
-		id: 3,
-		stars: 0,
-		progress: 0,
-		topic: {
-			id: 3,
-			name: 'Pronouns',
-			image: 'assets/images/lessons-images/lesson_image_english.png',
-			subject: {
-				id: 3,
-				name: 'Lengua Extranjera'
-			}
-		}
-	}
-];
-function SessionsProgressCard() {
-	return (
-		<Card>
-			<CardContent>
-				{/* Title */}
-				<Typography
+shapgvba FrffvbafCebterffPneq() {
+	erghea (
+		<Pneq>
+			<PneqPbagrag>
+				{/* Gvgyr */}
+				<Glcbtencul
 					variant="h5"
 					fontWeight="600"
 					mb={2}
